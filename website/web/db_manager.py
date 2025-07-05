ac9c1b67ecf28c977532f83d1924c4093554e9a3
inbarmiran <inbar.miran@campus.technion.ac.il>
2025-07-05 21:12:45 +0300

    plot operations

--- website/web/db_manager.py
+++ website/web/db_manager.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 from typing import Optional, Dict, Any, List
-from .models import File, Dataset, AnalysisResult, User
+from .models import File, Dataset, AnalysisResult, User, Plot
 
 class MongoDBManager:
     def __init__(self, uri: str = "mongodb://db:27017", db_name: str = "mydb"):
@@ -12,6 +12,7 @@ class MongoDBManager:
         self.datasets = self.db["datasets"]
         self.analysis = self.db["analysis_results"]
         self.users = self.db["users"]
+        self.plots = self.db["plots"]
 
 
 # ----- FILE OPERATIONS -----
@@ -133,3 +134,55 @@ class MongoDBManager:
         """
         result = self.users.delete_one({"_id": user_id})
         return result.deleted_count > 0
+
+# ----- PLOT OPERATIONS -----
+    def create_plot(self, plot: Plot) -> str:
+        """
+        Saves the plot to the collection and returns the plot id
+        :param plot: Plot object to save
+        :return: plot id
+        """
+        self.plots.insert_one(plot.to_dict())
+        return plot._id
+    
+    def get_plot(self, plot_id: str) -> Optional[Plot]:
+        """
+        Gets the plot from the collection
+        :param plot_id: ID of the plot to retrieve
+        :return: None if not found, otherwise rehydrates into a Plot object
+        """
+        data = self.plots.find_one({"_id": plot_id})
+        return Plot.from_dict(data) if data else None
+    
+    def delete_plot(self, plot_id: str) -> bool:
+        """
+        Deletes the plot from the collection
+        :param plot_id: ID of the plot to delete
+        :return: True if at least one doc was deleted, otherwise False
+        """
+        result = self.plots.delete_one({"_id": plot_id})
+        return result.deleted_count > 0
+    
+    def get_plots_for_user(self, user_id: str, only_presented: Optional[bool] = None) -> List[Plot]:
+        """
+        Returns a list of Plot objects belonging to the given user
+        :param user_id: ID of the user
+        :param only_presented: If True, return only presented images. If False, return only not presented images. If None, return all images.
+        :return: List of Plot objects
+        """
+        query: Dict[str, Any] = {"user_id": user_id}
+        if only_presented is not None:
+            query["is_presented"] = only_presented
+        
+        docs = self.plots.find(query)
+        return [Plot.from_dict(d) for d in docs]
+    
+    def present_plot(self, plot_id: str, is_presented: bool) -> bool:
+        """
+        Updates the is_presented field of a plot
+        :param plot_id: ID of the plot to update
+        :param is_presented: Boolean value to set
+        :return: True if at least one doc was modified, otherwise False
+        """
+        result = self.plots.update_one({"_id": plot_id}, {"$set": {"is_presented": is_presented}})
+        return result.modified_count > 0
