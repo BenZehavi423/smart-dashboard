391b21c0d100c0e5ee2aea4d21f4c65138e60234
BenZehavi <ben.zehavi423@gmail.com>
2025-08-29 15:13:58 +0300

    added a stress test. need to add more functions to locustfile.py to test all pages

--- website/web/__init__.py
+++ website/web/__init__.py
@@ -3,11 +3,20 @@ from .views import views
 import os
 from .db_manager import MongoDBManager
 from .auth import auth
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = "very-secret-key"  # 🔒 REPLACE in production
-    
+
+    # Initialize the rate limiter
+    limiter = Limiter(
+        get_remote_address,
+        app=app,
+        default_limits=["200 per day", "50 per hour"]
+    )
+
     # This line sets a default MongoDB URI only when the app is run locally outside of Docker.
     # It has no effect inside Docker, since the database URI is hardcoded in MongoDBManager as "mongodb://db:27017".
     os.environ.setdefault(
